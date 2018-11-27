c13d6c9df7cbbe0a8429d7ac283c7780f1ec3019
YahiaAngelo <yahia.mostafa.elsayed@gmail.com>
2018-11-27 02:42:47 +0200

    Clean

--- app/src/main/java/com/angelo/blackpearl/QueryUtils.java
+++ app/src/main/java/com/angelo/blackpearl/QueryUtils.java
@@ -2,67 +2,63 @@ package com.angelo.blackpearl;
 
 import android.util.Log;
 
-import com.google.firebase.database.ChildEventListener;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 
 public class QueryUtils {
 
 
+    private static final PostDatabase postDatabase = PostDatabase.getInstance(MainActivity.context);
 
 
-  private static final PostDatabase postDatabase = PostDatabase.getInstance(MainActivity.context);
-
-
-    private QueryUtils(){
+    private QueryUtils() {
 
     }
 
-    public static void extractPosts(){
+    public static void extractPosts() {
 
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-         FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("hamza");
+        String post = "post";
 
-         DatabaseReference myRef = database.getReference("hamza");
-         String post = "post";
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (int i = 0; i < dataSnapshot.getChildrenCount(); i++) {
+                    String title = (String) dataSnapshot.child(post + i).child("title").getValue();
+                    String desc = (String) dataSnapshot.child(post + i).child("desc").getValue();
+                    String link = (String) dataSnapshot.child(post + i).child("link").getValue();
+                    String imgLink = (String) dataSnapshot.child(post + i).child("img").getValue();
+                    Log.v("MyPost", "My post is:" + title + desc + link);
+                    Post post = new Post();
+                    post.setTitle(title);
+                    post.setDesc(desc);
+                    post.setLink(link);
+                    post.setImg(imgLink);
+                    post.setId(i);
+                    postDatabase.postDao().save(post);
+                }
+            }
 
-         myRef.addValueEventListener(new ValueEventListener() {
-             @Override
-             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                 for (int i =0; i<dataSnapshot.getChildrenCount(); i++){
-                     String title = (String) dataSnapshot.child(post + i).child("title").getValue();
-                     String desc = (String) dataSnapshot.child(post + i).child("desc").getValue();
-                     String link = (String) dataSnapshot.child(post + i).child("link").getValue();
-                     String imgLink = (String) dataSnapshot.child(post + i).child("img").getValue();
-                     Log.v("MyPost", "My post is:" + title + desc + link);
-                     Post post = new Post();
-                     post.setTitle(title);
-                     post.setDesc(desc);
-                     post.setLink(link);
-                     post.setImg(imgLink);
-                     post.setId(i);
-                     postDatabase.postDao().save(post);
-                 }
-             }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-             @Override
-             public void onCancelled(@NonNull DatabaseError databaseError) {
-
-             }
-         });
+            }
+        });
 
     }
+
+
 
 
 }
